adfa040075d905f06b53cb716847b574618861f5
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-08-01 21:16:59 +0530

    app

--- EcommerceApplication.DataAccess.Repository/obj/Debug/net6.0/EcommerceApplication.DataAccess.Repository.AssemblyInfo.cs
+++ EcommerceApplication.DataAccess.Repository/obj/Debug/net6.0/EcommerceApplication.DataAccess.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApplication.DataAccess.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b87b462f07aa45d3fac9f6bfbd2101444b873fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApplication.DataAccess.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApplication.DataAccess.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
